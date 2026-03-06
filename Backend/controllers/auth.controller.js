578de542ccd18e2bbec184710b146e1d946e1bcf
om bhagat <omibhagat09@gmail.com>
2026-03-06 20:55:10 +0530

    Added Login API

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -41,3 +41,35 @@ exports.signup = async (req, res) => {
 
 
 }
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({ message: "User not Found" })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Password" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d" }
+        )
+        res.status(200).json({
+            message: "Login Succesfully",
+            token,
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+
+};
