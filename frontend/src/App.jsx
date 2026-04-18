14200b3bec2693551bbfdb2beab3d9ab5ce19379
om bhagat <omibhagat09@gmail.com>
2026-04-18 22:46:36 +0530

    Fixed filename case imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
